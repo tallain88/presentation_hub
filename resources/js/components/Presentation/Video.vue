fa0794f673c415f1611f2d48ceeb6d877d01f566
Gumber <Gumber>
2021-04-24 16:59:23 -0400

    video source can be selected from local file

--- resources/js/components/Presentation/Video.vue
+++ resources/js/components/Presentation/Video.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="col-lg-6 container">
-        <video id="video" class="container-fluid" controls>
-            <source src="movie.mp4" type="video/mp4">
+        <video id="video" class="container-fluid" :key=source controls="true" >
+            <source :src=source type="video/mp4">
         </video>
     </div>
 </template>
@@ -16,9 +16,30 @@
 </style>
 
 <script>
+    import {eventBus} from "../../app";
     export default {
+        data() {
+            return {
+                source: '',
+            }
+        },
         mounted() {
             console.log('Component mounted.')
+        },
+        created() {
+            // attach event listener
+            eventBus.$on('source', this.updateSource);
+        },
+        beforeDestroy(){
+           // remove event listener
+           EventBus.$off("source", this.updateSource)
+        },
+        methods: {
+            // update video source and create URL object
+            updateSource(message){
+                this.source = URL.createObjectURL(message);
+                console.log(this.source);
+            }
         }
     }
 </script>
